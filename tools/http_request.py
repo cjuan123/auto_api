e99d19bcde23fe4487bd6dca8c3d4e96f57833ee
cjuan123 <cjuan_chen@163.com>
2019-05-27 17:51:23 +0800

    update 1 file

--- tools/http_request.py
+++ tools/http_request.py
@@ -10,19 +10,20 @@ import requests
 class Request:
 
     def post_request_data(self, _url, _data, _headers):
-        response = requests.post(url=_url, data=_data, headers=_headers)
-        print("【URL】：%s" % _url)
+        response = requests.post(url=_url, data=_data, headers=_headers, verify=False)
+        print("【request_URL】：%s" % _url)
         print("【status_code】: %d" % response.status_code)
         return response
 
     def post_request_json(self, _url, _json, _headers):
         response = requests.post(url=_url, json=_json, headers=_headers)
-        print("【URL】：%s" % _url)
+        print("【request_URL】：%s" % _url)
         print("【status_code】: %d" % response.status_code)
         return response
 
-    def get_request(self, _url, _headers, _data=None ):
-        response = requests.get(url=_url, data=_data, headers=_headers)
-        print("【URL】：%s" % _url)
+    def get_request(self, _url, _headers, _data=None):
+        response = requests.get(url=_url, params=_data, headers=_headers, verify=False)
+        print("【request_URL】：%s" % _url)
         print("【status_code】: %d" % response.status_code)
+
         return response
