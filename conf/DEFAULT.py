ef56bb6d63ef4ab4c4ca255af4c5f9920f6c453a
cjuan123 <cjuan_chen@163.com>
2019-05-27 17:42:57 +0800

    修改文件

--- conf/DEFAULT.py
+++ conf/DEFAULT.py
@@ -12,15 +12,11 @@ test1 = "https://test1.chinaylzl.com"
 test2 = "https://test2.chinaylzl.com"
 test3 = "https://test3.chinaylzl.com"
 
-PASSWORD_GOVERNMENT = "123qwe"
-PASSWORD_SAS = "123456"
-PASSWORD_PG = "Qsheal502"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
     "Connection": "keep-alive",
     "Accept-Language": "zh-CN,zh;q=0.9"
-    #"Cookie": "SPRING_SECURITY_REMEMBER_ME_COOKIE=MTM5OTk5OTk5OTI6MTU2MDEzMzM3MzE4MDowM2I3OTFiZGI3ZjQyNWIzODM0ZjkzNzFjYTlmNTJmYQ; CHINAYLZL_GROUP_YEAR_COOKIE=MjAxOToyOjE1NTg5MjM3NzM; TSESSIONID=f3dd40c5-9103-44ec-bae7-94326f68082a;"
 }
 
