707186a03e06126d84d45c910800179f4a606085
cjuan123 <cjuan_chen@163.com>
2019-05-27 17:41:31 +0800

    添加登录方法

--- conf/Login.py
+++ conf/Login.py
@@ -15,8 +15,10 @@ pc_host = DEFAULT.test3
 headers = DEFAULT.HEADERS
 
 def get_account(account, password):
+    """政府端登录"""
     def login(func):
         def inner(*args):
+            print("=============【政府端登录：%s】==================" % account)
             government_URL = government_host + "/Gover"
             request.get_request(_url=government_URL, _headers=headers)
             government_check = government_host + "/j_spring_security_check"
@@ -24,11 +26,28 @@ def get_account(account, password):
                 "mobile": account,
                 "password": password
             }
-            print(params)
             response = request.post_request_data(_url=government_check, _data=params, _headers=headers)
             headers["Cookie"] = response.request.headers["Cookie"]
-            print("====" + response.request.headers["Cookie"])
             func(*args)
         return inner
     return login
 
+
+def get_pc_account(account, password):
+    """大配餐企业端登录"""
+    def login(func):
+        def inner(*args):
+            print("=============【大配餐企业端登录：%s】==================" % account)
+            pc_URL = pc_host + "/login"
+            res = request.get_request(_url=pc_URL, _headers=headers)
+            pc_check = pc_host + "/checkUser"
+            param = {
+                "mobile": account,
+                "password": password
+            }
+            response = request.post_request_data(_url=pc_check, _data=param, _headers=headers)
+            headers["Cookie"] = "JSESSIONID:" + response.cookies.get_dict()["JSESSIONID"] + \
+                                ";USER_COOKIE_ID_meal" + response.cookies.get_dict()["USER_COOKIE_ID_meal"]
+            func(*args)
+        return inner
+    return login
