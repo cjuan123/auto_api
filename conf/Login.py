f670a9bbf1e5889be8b7d1536195d36cf2e2e302
cjuan123 <cjuan_chen@163.com>
2019-05-28 16:26:00 +0800

    app登录方法

--- conf/Login.py
+++ conf/Login.py
@@ -14,6 +14,7 @@ government_host = DEFAULT.test
 pc_host = DEFAULT.test3
 headers = DEFAULT.HEADERS
 
+
 def get_account(account, password):
     """政府端登录"""
     def login(func):
@@ -39,15 +40,31 @@ def get_pc_account(account, password):
         def inner(*args):
             print("=============【大配餐企业端登录：%s】==================" % account)
             pc_URL = pc_host + "/login"
-            res = request.get_request(_url=pc_URL, _headers=headers)
+            request.get_request(_url=pc_URL, _headers=headers)
             pc_check = pc_host + "/checkUser"
             param = {
                 "mobile": account,
                 "password": password
             }
             response = request.post_request_data(_url=pc_check, _data=param, _headers=headers)
-            headers["Cookie"] = "JSESSIONID:" + response.cookies.get_dict()["JSESSIONID"] + \
-                                ";USER_COOKIE_ID_meal" + response.cookies.get_dict()["USER_COOKIE_ID_meal"]
+            headers["Cookie"] = "JSESSIONID=" + response.cookies.get_dict()["JSESSIONID"] + \
+                                ";USER_COOKIE_ID_meal=" + response.cookies.get_dict()["USER_COOKIE_ID_meal"]
             func(*args)
         return inner
     return login
+
+
+def get_app_account(account, password):
+    def login(func):
+        def inner(*args):
+            _url = pc_host + "/api/login"
+            param = {
+                "mobile": account,
+                "password": password
+            }
+            response = request.post_request_data(_url=_url, _data=param, _headers=headers)
+            headers["Cookie"] = "USER_COOKIE_ID_meal=" + response.cookies.get_dict()["USER_COOKIE_ID_meal"]
+            func(*args)
+        return inner
+    return login
+
