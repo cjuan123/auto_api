30fd6947d50ceaf866410761f50468be4e1a8755
cjuan123 <cjuan_chen@163.com>
2019-06-06 17:59:40 +0800

    update file

--- test_case/test_transform/test_transform_gover/test_construction_app.py
+++ test_case/test_transform/test_transform_gover/test_construction_app.py
@@ -16,16 +16,18 @@ from source.Transform.construcotr_app import ConstructionApp
 class TestConstructionApp(unittest.TestCase):
     construction_app = ConstructionApp()
     id_card = IDCard().idCard(75, 2)
+    con_emp_id = []
     con_emp_account = "1525621" + str(random.randint(1000, 9999))
     con_emp_name = "api施员工" + str(random.randint(1000, 9999))
 
-    @Login.get_construction_web("13633269651", "123456")
+    @Login.get_construction_web("13633268550", "123456")
     def test_add_employee(self):
+        print(self.con_emp_name)
         param = {
             "account": self.con_emp_account,
             "addressDetail": "api测试详细地址",
             "annex": "apiannex",
-            "bid": 45,
+            "bid": 44,
             "idCard": self.id_card,
             "name": self.con_emp_name,
             "password": "123456",
@@ -34,12 +36,40 @@ class TestConstructionApp(unittest.TestCase):
             }
         response = self.construction_app.add_employee(param=param)
         print(response.json())
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(0, response.json()["status"])
+        self.assertEqual("添加成功", response.json()["message"])
 
+    @Login.get_construction_web("13633261866", "123456")
+    def test_employee_list(self):
+        """施工单位：查询员工列表"""
+        param = {
+            "name": self.con_emp_name,
+            "pageNow": 1
+        }
+
+        response = self.construction_app.employee_list(param=param)
+        print(response.json())
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(0, response.json()["status"])
+        self.assertEqual("操作成功", response.json()["message"])
+        print(len(response.json()["data"]["records"]))
+        self.con_emp_id.append(response.json()["data"]["records"][0]["id"])
+        print(self.con_emp_id[0])
+
+    @Login.get_construction_web("13633269651", "123456")
     def test_get_task(self):
         """施工app：获取任务"""
         param = {
-            "eId": "",
-            "recordId": ""
+            "eId": self.con_emp_id[0],
+            "recordId": 36
         }
         response = self.construction_app.get_task(param=param)
         print(response.json())
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(0, response.json()["status"])
+        self.assertEqual("领取成功", response.json()["message"])
+
+
+if __name__ == '__main__':
+    unittest.main()
