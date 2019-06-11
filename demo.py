ce9bd693189723a8d50d387d794537a5963d00ab
cjuan123 <cjuan_chen@163.com>
2019-06-11 09:44:11 +0800

    update Login.py

--- demo.py
+++ demo.py
@@ -8,3 +8,35 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]))
 import requests
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+    "Connection": "keep-alive",
+    "Accept-Language": "zh-CN,zh;q=0.9"
+}
+
+
+# _url = "https://test.chinaylzl.com/Gover"
+# requests.get(_url, headers=headers, verify=False)
+#
+# _check = "https://test.chinaylzl.com/j_spring_security_check"
+# params = {
+#     "mobile": "18048054262",
+#     "password": "123qwe"
+#           }
+# res = requests.post(url=_check, data=params, headers=headers, verify=False)
+#
+# headers["Cookie"] = res.request.headers["Cookie"]
+# print(headers)
+#
+# url = "https://test.chinaylzl.com/surveyUser/listUsersNew"      # 人员批量充值
+# param = {
+#     "idCard": "711557195305306788",
+#     "pageIndex": 1,
+#     "isBind": ""
+# }
+# res = requests.post(url=url, headers=headers, data=param, verify=False)
+# print(res.status_code)
+# print(res.json())
+
+
