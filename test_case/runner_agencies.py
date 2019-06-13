7c52e186d96a13398f68266416c56a0d3cd9ca6d
cjuan123 <cjuan_chen@163.com>
2019-06-13 20:41:57 +0800

    评估 流程 报告

--- test_case/runner_agencies.py
+++ test_case/runner_agencies.py
@@ -2,9 +2,9 @@
 """
 @version: 1.0
 @author: chenj
-@file: run_govern_house.py
-@time: 2019/6/13 14:49
-@desc：运行适老化改造流程脚本，并生成测试报告
+@file: runner_agencies.py
+@time: 2019/6/13 20:34
+@desc：运行评估流程脚本，并生成测试报告
 """
 import unittest
 import os, time
@@ -14,7 +14,7 @@ from runner import HTMLTestRunnerNew
 # 测试报告存放地址
 reports_path = os.path.join(FilePath().reports_path(), "test_reports_house_%s.html" % time.strftime('%Y_%m_%d'))
 
-discover = unittest.defaultTestLoader.discover(r"F:\auto_api\test_case\test_house", pattern="test_house_flow.py")
+discover = unittest.defaultTestLoader.discover(r"F:\auto_api\test_case\test_agencies", pattern="test_*.py")
 
 root_path = os.path.dirname(os.path.abspath(".."))
 file = open(os.path.join(root_path, reports_path), "wb+")
