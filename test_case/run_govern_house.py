4860e69c7dfd0fadc3efb8c7988172c407382cef
cjuan123 <cjuan_chen@163.com>
2019-06-13 20:50:05 +0800

    测试报告

--- test_case/run_govern_house.py
+++ test_case/run_govern_house.py
@@ -13,10 +13,13 @@ from runner import HTMLTestRunnerNew
 
 # 测试报告存放地址
 reports_path = os.path.join(FilePath().reports_path(), "test_reports_house_%s.html" % time.strftime('%Y_%m_%d'))
+cast_path = os.path.join(FilePath().case_path(), "test_house")
+# discover = unittest.defaultTestLoader.discover(r"F:\auto_api\test_case\test_house", pattern="test_house_flow.py")
+discover = unittest.defaultTestLoader.discover(cast_path, pattern="test_house_flow.py")
 
-discover = unittest.defaultTestLoader.discover(r"F:\auto_api\test_case\test_house", pattern="test_house_flow.py")
 
 root_path = os.path.dirname(os.path.abspath(".."))
 file = open(os.path.join(root_path, reports_path), "wb+")
 runner = HTMLTestRunnerNew.HTMLTestRunner(stream=file)
 runner.run(discover)
+
--- test_case/runner_agencies.py
+++ test_case/runner_agencies.py
@@ -12,9 +12,12 @@ from tools.file_path import FilePath
 from runner import HTMLTestRunnerNew
 
 # 测试报告存放地址
-reports_path = os.path.join(FilePath().reports_path(), "test_reports_house_%s.html" % time.strftime('%Y_%m_%d'))
+reports_path = os.path.join(FilePath().reports_path(), "test_reports_agencies_%s.html" % time.strftime('%Y_%m_%d'))
 
-discover = unittest.defaultTestLoader.discover(r"F:\auto_api\test_case\test_agencies", pattern="test_*.py")
+cast_path = os.path.join(FilePath().case_path(), "test_agencies")
+# discover = unittest.defaultTestLoader.discover(r"F:\auto_api\test_case\test_agencies", pattern="test_*.py")
+discover = unittest.defaultTestLoader.discover(cast_path, pattern="test_*.py")
+
 
 root_path = os.path.dirname(os.path.abspath(".."))
 file = open(os.path.join(root_path, reports_path), "wb+")
